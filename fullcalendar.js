db02aff301e460038106257aaf628906baa9b4db
shihaoking <shihaoking@gmail.com>
2014-06-04 17:08:31 +0800

    增加小日历功能

--- fullcalendar.js
+++ fullcalendar.js
@@ -1076,7 +1076,7 @@ function Header(calendar, options) {
 		tm = options.theme ? 'ui' : 'fc';
 		var sections = options.header;
 		if (sections) {
-			element = $("<table class='fc-header' style='width:100%'/>")
+			element = $("<table class='fc-header'/>")
 				.append(
 					$("<tr/>")
 						.append(renderSection('left'))
