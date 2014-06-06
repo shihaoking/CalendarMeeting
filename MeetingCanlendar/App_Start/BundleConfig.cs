f104fa8fd8dd3345f1c0a34cc8ba25b984db0073
shihaoking <shihaoking@gmail.com>
2014-06-06 23:25:26 +0800

    增加.NET版本

---
+++ MeetingCanlendar/App_Start/BundleConfig.cs
@@ -0,0 +1,25 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace MeetingCanlendar
+{
+    public class BundleConfig
+    {
+        // 有关 Bundling 的详细信息，请访问 http://go.microsoft.com/fwlink/?LinkId=254725
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                        "~/Scripts/jquery-{version}.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jqueryui").Include(
+                        "~/Scripts/jquery-ui-{version}.js"));
+
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                "~/Content/css/main.css",
+                "~/Content/css/datepicker.css",
+                "~/Content/css/chosen.css",
+                "~/Content/css/fullcalendar.css"));
+        }
+    }
+}
---
+++ MeetingCanlendar/Controllers/MeetingController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MeetingCanlendar.Controllers
+{
+    public class MeetingController : Controller
+    {
+        //
+        // GET: /Meeting/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
